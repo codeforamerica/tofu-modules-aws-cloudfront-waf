6960d2abb53db8257307cf76395ebf7726265069
James Armes <jarmes@codeforamerica.org>
2025-02-06 12:36:35 -0500

    fix!: Allow GenericLFI_Body matches for known upload paths.

--- uploads.tf
+++ uploads.tf
@@ -3,7 +3,7 @@ resource "aws_wafv2_rule_group" "uploads" {
 
   name_prefix = "${local.prefix}-waf-uploads-"
   scope       = "CLOUDFRONT"
-  capacity    = var.upload_rules_capacity == null ? 9 * length(var.upload_paths) : var.upload_rules_capacity
+  capacity    = var.upload_rules_capacity == null ? 12 * length(var.upload_paths) : var.upload_rules_capacity
 
   visibility_config {
     cloudwatch_metrics_enabled = true
@@ -167,6 +167,60 @@ resource "aws_wafv2_rule_group" "uploads" {
     visibility_config {
       cloudwatch_metrics_enabled = true
       metric_name                = "${local.prefix}-waf-request-xss"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # Block local file inclusion (LFI) requests, unless it was triggered by a file
+  # upload.
+  rule {
+    name     = "${local.prefix}-waf-request-lfi"
+    priority = 3
+
+    action {
+      block {}
+    }
+
+    statement {
+      and_statement {
+        statement {
+          label_match_statement {
+            key   = "awswaf:managed:aws:core-rule-set:GenericLFI_Body"
+            scope = "LABEL"
+          }
+        }
+
+        # Create a NOT statement for each of the upload paths. We'll block the
+        # request if it doesn't match any of the paths.
+        dynamic "statement" {
+          for_each = var.upload_paths
+
+          content {
+            not_statement {
+              statement {
+                byte_match_statement {
+                  positional_constraint = statement.value.constraint
+                  search_string         = statement.value.path
+
+                  field_to_match {
+                    uri_path {}
+                  }
+
+                  text_transformation {
+                    priority = 0
+                    type     = "NONE"
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${local.prefix}-waf-request-lfi"
       sampled_requests_enabled   = true
     }
   }
