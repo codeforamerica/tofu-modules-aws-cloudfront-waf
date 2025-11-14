1ce0a5631e88db44a1c2db78a99a0cf2622b6447
James Armes (they/them) <jarmes@codeforamerica.org>
2025-11-14 13:02:53 -0500

    feat: Support setting no subdomain. (#36)

--- locals.tf
+++ locals.tf
@@ -1,12 +1,12 @@
 locals {
-  fqdn      = "${local.subdomain}.${var.domain}"
-  subdomain = var.subdomain == "" ? var.environment : var.subdomain
+  fqdn      = join(".", compact([local.subdomain, var.domain]))
+  subdomain = var.subdomain != null ? var.subdomain : var.environment
   # If an origin ALB ARN is provided, use its DNS name; otherwise, use the
   # provided origin domain or construct one.
   origin_domain = (var.origin_alb_arn != null
     ? data.aws_lb.origin["this"].dns_name
-    : (var.origin_domain != "" ? var.origin_domain : join(".", ["origin", local.subdomain, var.domain]))
+    : (var.origin_domain != "" ? var.origin_domain : join(".", compact(["origin", local.subdomain, var.domain])))
   )
   prefix = "${var.project}-${var.environment}"
-  tags   = merge(var.tags, { domain : "${local.subdomain}.${var.domain}" })
+  tags   = merge(var.tags, { domain : local.fqdn })
 }
